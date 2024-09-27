b29e20e32047758524261eae34a9f6fd57edf06e
Akorede <akoredeadewole8@gmail.com>
2024-09-27 17:00:58 +0100

    added week5 friday class

--- Jobs API/middlewares/errorHandler/index.js
+++ Jobs API/middlewares/errorHandler/index.js
@@ -1,20 +1,47 @@
 import ApiError from "./api-error.js"
 import { errorResponse } from "../../utils/responses.js"
+import jsonwebtoken from 'jsonwebtoken';
+import { StatusCodes } from "http-status-codes";
+import logger from "../../utils/logger.js";
+
+const { JsonWebTokenError } = jsonwebtoken;
+
 
 const errorHandler = (error, req, res, next) => {
     let message = "Request failed. Try again later"
-    let errCode = 500
+    let errCode = StatusCodes.INTERNAL_SERVER_ERROR // 500
 
 
     if (error instanceof ApiError){
         message = error.message
         errCode = error.code
+    }else if (error instanceof JsonWebTokenError){
+        message = error.message
+        errCode = StatusCodes.FORBIDDEN //403
+    }else if (
+        error instanceof SyntaxError ||
+        error instanceof EvalError ||
+        error instanceof RangeError ||
+        error instanceof ReferenceError ||
+        error instanceof TypeError ||
+        error instanceof URIError
+    ){
+        message = error.message
+        errCode = StatusCodes.BAD_REQUEST //400
     }else if (error instanceof Error){
-        message = 'probably a mongoose error'
-        errCode = 422
+        if (error.name === 'ValidationError'){
+            console.log(JSON.stringify(error.errors))
+            message = Object.values(error.errors).map(err => err.message).join(' ')
+            errCode = StatusCodes.UNPROCESSABLE_ENTITY
+        }else{
+            message = error.message
+            errCode = StatusCodes.BAD_GATEWAY
+        }
     }
 
+    // log the error and return the response
 
+    logger.error(`[${req.method} ${req.url}] ${typeof message === 'string' ? message : JSON.stringify(message)}`);
     errorResponse(res, errCode, message)
 
 
